da70bdee73a32a19f0c7c6cfe2203b9e1a41291f
Habibu Mamudu <habibumamudu@gmail.com>
2023-08-09 17:49:25 +0000

    Possible combination

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,11 +2,11 @@
 
 for i in range(0, 9):
     for j in range(0, 10):
-        if j == i:
+        if j == i or i > j and (i * 10 + j) % 10 == j:
             continue
-        if not i * j ==  72:
-            if not i == j:
-                print(f"{i:d}{j:d}".format(i, j), end=', ')
         else:
-            print(f"{i:d}{j:d}".format(i, j), end='')
+            if not j * i == 72:
+                print(f"{i:d}{j:d}".format(i, j), end=', ')
+            else:
+                print(f"{i:d}{j:d}".format(i, j), end='')
 print("")
