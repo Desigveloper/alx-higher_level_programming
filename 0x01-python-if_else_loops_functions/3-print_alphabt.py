4f135b97d1f0e82311fde1c00076270fcd27ad55
Habibu Mamudu <habibumamudu@gmail.com>
2023-08-09 06:25:52 +0000

    task 3 updated

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 for i in range(97, 123):
-    if i == 'e' or i == 'q':
+    if chr(i) == 'e' or chr(i) == 'q':
 	    continue
     else:
 	    print(f'{chr(i)}'.format(chr(i)), end='')
