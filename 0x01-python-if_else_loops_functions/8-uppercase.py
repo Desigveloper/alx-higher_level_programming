a899ef93787cecb7e32e8e739c6ffcdf8a4b1e1d
Habibu Mamudu <habibumamudu@gmail.com>
2023-08-10 17:46:38 +0000

    Convert all letters to uppercase

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,7 +3,7 @@
 def uppercase(str):
     for letter in str:
         if ord(letter) >= 97 and ord(letter) <= 122:
-           upper_case = chr(ord(letter) - 32)
+            upper_case = chr(ord(letter) - 32)
         else:
             upper_case = letter
         print(f'{upper_case}'.format(upper_case), end='')
