11d82ffef3a2fa1f9875f7c29dbb559ae7c13b1b
Habibu Mamudu <habibumamudu@gmail.com>
2023-08-11 17:33:01 +0000

    remove a letter at index

---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+def remove_char_at(str, n):
+    new_str = ''
+    i = 0
+    j = 0
+
+    for l in str:
+        i += 1
+
+    while (j < i):
+        if not j == n:
+            new_str += str[j]
+        j += 1
+
+    return new_str
