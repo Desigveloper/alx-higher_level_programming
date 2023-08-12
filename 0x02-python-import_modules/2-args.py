a2463680646d18cd3e7cee74493bea8c28849b3a
Habibu Mamudu <habibumamudu@gmail.com>
2023-08-12 04:50:50 +0000

    script dynamic: prints number and list of arguments

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+def count_argv():
+    from sys import argv
+
+    i = 1
+    arg_len = len(argv) - 1
+
+    if arg_len == 1:
+        print("{0} {1}:".format(arg_len, "argument"))
+    else:
+        print("{0} {1}:".format(arg_len, "arguments"))
+
+    while (i <= arg_len):
+        print("{0}: {1}".format(i, argv[i]))
+        i += 1
+
+
+if __name__ == '__main__':
+    count_argv()
