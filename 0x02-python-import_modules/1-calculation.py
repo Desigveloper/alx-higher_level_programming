d0e5a20611ff6f3a4e69cdfb5cd5aef5be6cb77f
Habibu Mamudu <habibumamudu@gmail.com>
2023-08-12 06:15:57 +0000

    Update doc

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -6,11 +6,11 @@ if __name__ == '__main__':
     a = 10
     b = 5
     sum = calculator.add(a, b)
-    sub = calculator.sub(a,b)
+    sub = calculator.sub(a, b)
     mul = calculator.mul(a, b)
     div = calculator.div(a, b)
 
-    print("{0} + {1} = {2}".format(a, b ,sum))
-    print("{0} - {1} = {2}".format(a, b ,sub))
-    print("{0} * {1} = {2}".format(a, b ,mul))
-    print("{0} / {1} = {2}".format(a, b ,div))
+    print("{0} + {1} = {2}".format(a, b, sum))
+    print("{0} - {1} = {2}".format(a, b, sub))
+    print("{0} * {1} = {2}".format(a, b, mul))
+    print("{0} / {1} = {2}".format(a, b, div))
