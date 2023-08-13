d7004c95ce50b00d15ec45c555b483a421fd9cf4
Habibu Mamudu <habibumamudu@gmail.com>
2023-08-13 18:19:24 +0000

    custom calculator

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -21,21 +21,22 @@ def handle_operation():
     if not args_len == 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         return 1
-    elif argv[2] != '+' and argv[2] != '-' and argv[2] != '*' and argv[2] != '/': 
+
+    a = int(argv[1])
+    operator = argv[2]
+    b = int(argv[3])
+
+    if operator == '+':
+        print("{0} + {1} = {2}".format(a, b, calc.add(a, b)))
+    elif operator == '-':
+        print("{0} - {1} = {2}".format(a, b, calc.sub(a, b)))
+    elif operator == '*':
+        print("{0} * {1} = {2}".format(a, b, calc.mul(a, b)))
+    elif operator == '/':
+        print("{0} / {1} = {2}".format(a, b, calc.div(a, b)))
+    else:
         print("Unknown operator. Available operators: +, -, * and /")
         return 1
-    else:
-        a = int(argv[1])
-        b = int(argv[3])
-
-        if argv[2] == '+':
-            print("{0} + {1} = {2}".format(a, b, calc.add(a, b)))
-        elif argv[2] == '-':
-            print("{0} - {1} = {2}".format(a, b, calc.sub(a, b)))
-        elif argv[2] == '*':
-            print("{0} * {1} = {2}".format(a, b, calc.mul(a, b)))
-        else:
-            print("{0} / {1} = {2}".format(a, b, calc.div(a, b)))
 
 
 
