2a7c3384a2eed345746ad918fd029eda7e314038
Habibu Mamudu <habibumamudu@gmail.com>
2023-08-13 20:56:38 +0000

    custom calculator

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -15,26 +15,26 @@ def handle_operation():
     from sys import argv, exit
 
     args_len = len(argv) - 1
-    ope = argv[2]
 
     if not args_len == 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
-    elif ope != '+' and ope != '-' and ope != '*' and ope != '/':
-        print("Unknown operator. Available operators: +, -, * and /")
-        exit(1)
-
-    a = int(argv[1])
-    b = int(argv[3])
-
-    if argv[2] == '+':
-        print("{0} + {1} = {2}".format(a, b, calc.add(a, b)))
-    elif argv[2] == '-':
-        print("{0} - {1} = {2}".format(a, b, calc.sub(a, b)))
-    elif argv[2] == '*':
-        print('{0} * {1} = {2}'.format(a, b, calc.mul(a, b)))
     else:
-        print("{0} / {1} = {2}".format(a, b, calc.div(a, b)))
+        op = argv[2]
+        a = int(argv[1])
+        b = int(argv[3])
+
+        if op == '+':
+            print("{0} + {1} = {2}".format(a, b, calc.add(a, b)))
+        elif op == '-':
+            print("{0} - {1} = {2}".format(a, b, calc.sub(a, b)))
+        elif op == '*':
+            print('{0} * {1} = {2}'.format(a, b, calc.mul(a, b)))
+        elif op == '/':
+            print("{0} / {1} = {2}".format(a, b, calc.div(a, b)))
+        else:
+            print("Unknown operator. Available operators: +, -, * and /")
+            exit(1)
 
 
 if __name__ == '__main__':
