5244027fcf5a021ae08c2af6120115afd29f2dc1
Habibu Mamudu <habibumamudu@gmail.com>
2023-08-13 18:51:26 +0000

    custom calculator

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,20 +1,9 @@
 #!/usr/bin/python3
+from sys import argv
 
 def handle_operation():
     import calculator_1 as calc
     from sys import argv
-
-
-    """
-
-    imports all functions from the file calculator_1.py and handles basic operations.
-
-    Parameters:
-        None
-
-    Returns:
-        1 if error
-    """
 
     args_len = len(argv) - 1
 
@@ -23,21 +12,22 @@ def handle_operation():
         return 1
 
     a = int(argv[1])
-    operator = argv[2]
     b = int(argv[3])
+    operator = argv[2]
 
     if operator == '+':
         print("{0} + {1} = {2}".format(a, b, calc.add(a, b)))
     elif operator == '-':
         print("{0} - {1} = {2}".format(a, b, calc.sub(a, b)))
-    elif operator == '*':
-        print("{0} * {1} = {2}".format(a, b, calc.mul(a, b)))
     elif operator == '/':
         print("{0} / {1} = {2}".format(a, b, calc.div(a, b)))
     else:
-        print("Unknown operator. Available operators: +, -, * and /")
-        return 1
-
+        try:
+            result = eval("{0} {1} {2}".format(a, operator, b))
+            print("{0} {1} {2} = {3}".format(a, operator, b, result))
+        except:
+            print("Unknown operator. Available operators: +, -, * and /")
+            return 1
 
 
 if __name__ == '__main__':
