983dbb163157b3fbc805c0db0b5095f045b5d675
Habibu Mamudu <habibumamudu@gmail.com>
2023-08-13 19:07:38 +0000

    custom calculator

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,33 +1,42 @@
 #!/usr/bin/python3
-from sys import argv
 
 def handle_operation():
     import calculator_1 as calc
     from sys import argv
+
+
+    """
+
+    imports all functions from the file calculator_1.py and handles basic operations.
+
+    Parameters:
+        None
+
+    Returns:
+        1 if error
+    """
 
     args_len = len(argv) - 1
 
     if not args_len == 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         return 1
+    elif argv[2] != '+' and argv[2] != '-' and argv[2] != '*' and argv[2] != '/': 
+        print("Unknown operator. Available operators: +, -, * and /")
+        return 1
 
     a = int(argv[1])
     b = int(argv[3])
-    operator = argv[2]
 
-    if operator == '+':
+    if argv[2] == '+':
         print("{0} + {1} = {2}".format(a, b, calc.add(a, b)))
-    elif operator == '-':
+    elif argv[2] == '-':
         print("{0} - {1} = {2}".format(a, b, calc.sub(a, b)))
-    elif operator == '/':
-        print("{0} / {1} = {2}".format(a, b, calc.div(a, b)))
+    elif argv[2] == '*':
+        print('{0} * {1} = {2}'.format(a, b, calc.mul(a, b)))
     else:
-        try:
-            result = eval("{0} {1} {2}".format(a, operator, b))
-            print("{0} {1} {2} = {3}".format(a, operator, b, result))
-        except:
-            print("Unknown operator. Available operators: +, -, * and /")
-            return 1
+        print("{0} / {1} = {2}".format(a, b, calc.div(a, b)))
+
 
 
 if __name__ == '__main__':
