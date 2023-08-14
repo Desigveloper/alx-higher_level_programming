a13aac4dd58960e95b060959c4cbaedaf1c080c5
Habibu Mamudu <habibumamudu@gmail.com>
2023-08-14 14:24:58 +0000

    print all interger in a list

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def print_list_integer(my_list=[]):
+    """
+	prints all integers in a given list
+
+	Parameter:
+	    my_list - list to be printed
+
+	Return:
+	     none
+	"""
+    for el in my_list:
+        print("{:d}".format(el))
