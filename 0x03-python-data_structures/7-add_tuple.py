ce802d7b034fb4e25ca31bc5f2b6e89e967567d9
Habibu Mamudu <habibumamudu@gmail.com>
2023-08-16 07:43:25 +0000

    updated 7-tuple

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -3,6 +3,12 @@
 def add_tuple(tuple_a=(), tuple_b=()):
     tuple_c = ()
 
+    if not tuple_a:
+        tuple_a = (0,)
+
+    if not tuple_b:
+        tuple_b = (0,)
+
     for i in range(2):
         if len(tuple_a) < 2:
             tuple_a += (0,)
