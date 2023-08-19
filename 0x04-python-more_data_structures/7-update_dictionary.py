a25f03f969eb5c59021c1a4203d9e9ed03b4761b
Habibu Mamudu <habibumamudu@gmail.com>
2023-08-19 04:47:20 +0000

    update keys and value in a dictionary

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+def update_dictionary(a_dictionary, key, value):
+    # Updates an existing key or adds a new key/value pair to the dictionary
+    a_dictionary[key] = value
+
+    return a_dictionary
