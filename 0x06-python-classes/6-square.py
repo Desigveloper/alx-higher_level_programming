0c5e5d064e37d64d78b0481bf41789569aba54c4
Habibu Mamudu <habibumamudu@gmail.com>
2023-08-30 18:47:08 +0000

    Printing cordinates of square with #

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -58,12 +58,12 @@ class Square:
             ValueError: If `size` is less than 0.
 
         """
-        if not isinstance(value, tuple) and len(value) == 2 and \
+        if isinstance(value, tuple) and len(value) == 2 and \
                 isinstance(value[0], int) and isinstance(value[1], int) \
                 and value[0] >= 0 and value[1] >= 0:
-            raise TypeError("position must be a tuple of 2 positive integers")
-        else:
             self.__position = value
+        else:
+            raise TypeError("position must be a tuple of 2 positive integers")
 
     def area(self):
         """
