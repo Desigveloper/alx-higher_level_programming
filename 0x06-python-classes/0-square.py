0a09591d188ab6773db6f910277fcaa01ab9b33a
Habibu Mamudu <habibumamudu@gmail.com>
2023-08-30 11:28:48 +0000

    Square class

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 """
  This module defines a class representing a Square
-""
+"""
+
 
 class Square:
+    """This class is the template/skeleton for the Square
     """
-    This class is the template/skeleton for the Square
-    ""
-
     pass
