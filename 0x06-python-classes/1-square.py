abfaffe29e056e0ebc71163265793799db8d1003
Habibu Mamudu <habibumamudu@gmail.com>
2023-08-30 11:45:30 +0000

    private size initialization

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python3
+"""
+ This module defines a class representing a Square
+"""
+
 
 class Square:
+    """Implementation of the Square
+    """
     def __init__(self, size):
+        """
+            Initializes an instance of the Square classs
+        """
         self.__size = size
