fa59d1499c5fc5d4be77ba6970fd1d6dd96eded4
Habibu Mamudu <habibumamudu@gmail.com>
2023-09-05 07:28:23 +0000

    Compare rectangles

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -10,6 +10,7 @@ class Rectangle:
     """
 
     number_of_instances = 0
+    print_symbol = '#'
 
     def __init__(self, width=0, height=0):
         self.width = width
@@ -56,33 +57,72 @@ class Rectangle:
             Area of the rectangle
     """
     def perimeter(self):
+        """
+        Calculate the perimeter of the rectangle.
+
+        Returns:
+            The perimeter of the rectangle as an integer.
+        """
         if self.__width == 0 or self.__height == 0:
             return 0
         return 2 * (self.__width + self.__height)
 
     def __str__(self):
+        """
+        Returns a string representation of the rectangle.
+        """
         if self.__width == 0 or self.__height == 0:
             return ""
         rectangle = ""
         for _ in range(self.__height):
-            rectangle += "#" * self.__width + "\n"
+            rectangle += str(self.print_symbol) * self.__width + "\n"
         return rectangle[:-1]
 
     def __repr__(self):
-        return f"Rectangle({self.__width}, {self.__height})"
+        """
+        Returns a string representation of the Rectangle object.
+
+        :return: A string representation of the Rectangle object.
+        """
+        return "Rectangle({}, {})".format(self.__width, self.__height)
 
     def __del__(self):
         """
-        Delete an instance of the Rectangle class.
+        Destructor method for the Rectangle class.
 
-        This method decrements the number of Rectangle instances by 1
-        and prints a farewell message.
+        Decreases the number of instances of the Rectangle class by 1.
+        Prints a farewell message.
 
-        Parameters:
-            self (Rectangle): The instance of the class to be deleted.
-
+        Args:
+            self: The instance of the Rectangle class.
         Returns:
             None
         """
         Rectangle.number_of_instances -= 1
         print("Bye rectangle...")
+
+    @staticmethod
+    def bigger_or_equal(rect_1, rect_2):
+        """
+        Compares two rectangles and returns the area of the larger rectangle.
+
+        Parameters:
+            rect_1 (Rectangle): The first rectangle object to compare.
+            rect_2 (Rectangle): The second rectangle object to compare.
+
+        Raises:
+            TypeError: If rect_1 or rect_2 is not an instance of Rectangle.
+
+        Returns:
+            int: The area of the larger rectangle.
+        """
+        if not isinstance(rect_1, Rectangle):
+            raise TypeError("rect_1 must be an instance of Rectangle")
+        elif not isinstance(rect_2, Rectangle):
+            raise TypeError("rect_2 must be an instance of Rectangle")
+        else:
+            area_1 = rect_1.area()
+            area_2 = rect_2.area()
+            if area_1 < area_2:
+                return area_2
+            return area_1
