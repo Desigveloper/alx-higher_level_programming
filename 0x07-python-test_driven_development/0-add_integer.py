987107168ec6951c1031e19835e5865bc5441d2b
Habibu Mamudu <habibumamudu@gmail.com>
2023-09-08 08:44:58 +0000

    Integers addition

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """Integer Addition Module"""
 
+
 def add_integer(a, b=98):
-    """ 
+    """
     Adds two integers
-    
     Parameter:
     a(int): The first integer
     b (int): The second integer that defaults to 98 is no arg passes
