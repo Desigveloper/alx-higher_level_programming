3333f8692c88824c95c7a28c9a6a86e359c9880b
Habibu Mamudu <habibumamudu@gmail.com>
2023-09-08 08:43:02 +0000

    Integers addition

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,7 +1,17 @@
 #!/usr/bin/python3
+"""Integer Addition Module"""
 
 def add_integer(a, b=98):
-    """ Adds two integers """
+    """ 
+    Adds two integers
+    
+    Parameter:
+    a(int): The first integer
+    b (int): The second integer that defaults to 98 is no arg passes
+
+    Returns:
+    int: The sum of a and b
+    """
 
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
